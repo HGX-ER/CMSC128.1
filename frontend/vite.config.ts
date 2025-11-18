1749583d2ff0a7ea19c933b91a7e24d35d9cfe6d
Gelo <gmfernando@up.edu.ph>
2025-11-18 14:14:25 +0800

    tried adding vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base: process.env.VITE_BASE_PATH || "/CMSC128.1",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
